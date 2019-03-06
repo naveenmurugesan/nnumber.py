47b86002560c1b8301756feae76ca06d43c7c697
naveenmurugesan <naveenmurugesan4@gmail.com>
2019-03-06 12:45:47 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,15 @@
+num1 = int(input())
+
+if num1 > 1:
+  
+   for i in range(2,num1):
+       if (num1 % i) == 0:
+           print("no")
+           
+           break
+   else:
+       print("yes")
+       
+
+else:
+   print("no")
