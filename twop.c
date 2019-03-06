1b31ae194e57739a647fe6e03030ae4c87dc6abb
naveenmurugesan <naveenmurugesan4@gmail.com>
2019-03-06 13:23:40 +0530

    Update twop.c

--- twop.c
+++ twop.c
@@ -3,9 +3,9 @@ int main()
 {
     int n1, n2, i, flag;
 
-    printf("Enter two positive integers: ");
+    printf("");
     scanf("%d %d", &n1, &n2);
-    printf("Prime numbers between %d and %d are: ", n1, n2);
+    
 
     for(i=n1+1; i<n2; ++i)
     {
